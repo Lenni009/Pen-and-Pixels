b05e812e20725df4c3a73b9c9cf62163abb808c5
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-23 15:39:18 +0100

    basic Vitepress template

---
+++ .vitepress/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Studioprojekt ME21b",
+  description: "Wiki für das Studioprojekt der ME21b",
+  base: '/Studioprojekt2024/',
+  srcDir: 'pages',
+  cleanUrls: true,
+  head: [
+    ['link', {rel: 'icon', href: '/Studioprojekt2024/favicon.ico'}]
+  ],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Lenni009/Studioprojekt2024' }
+    ]
+  }
+})
