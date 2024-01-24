37cd7385d0bd1d7c524cbdaf494669beaa9ea156
GregsonderGrauwerdene <157585726+GregsonderGrauwerdene@users.noreply.github.com>
2024-01-24 14:05:04 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@616efb18230e34571e4e21a2e99859feb53c0535 🚀

---
+++ assets/welt.md.VgNyQxH6.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as n,V as i}from"./chunks/framework.mleWmeBo.js";const b=JSON.parse('{"title":"Welt","description":"","frontmatter":{},"headers":[],"relativePath":"welt.md","filePath":"welt.md"}'),t={name:"welt.md"},a=i("",14),d=[a];function l(h,s,m,c,o,u){return n(),r("div",null,d)}const f=e(t,[["render",l]]);export{b as __pageData,f as default};
