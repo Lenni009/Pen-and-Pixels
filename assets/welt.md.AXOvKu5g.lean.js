217d64835df1e146e43ecd1ea305655e252a3a43
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-24 14:57:43 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@926b0e4e510b8eb103a1271b9296ce0ac7485e9e 🚀

--- assets/welt.md.AXOvKu5g.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as n,V as i}from"./chunks/framework.mleWmeBo.js";const b=JSON.parse('{"title":"Regionen des Weltenbaums","description":"","frontmatter":{},"headers":[],"relativePath":"welt.md","filePath":"welt.md"}'),t={name:"welt.md"},a=i("",14),d=[a];function l(h,s,m,o,u,c){return n(),r("div",null,d)}const f=e(t,[["render",l]]);export{b as __pageData,f as default};
