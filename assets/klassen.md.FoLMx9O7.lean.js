8fae73cd29173c3f623c904c7993b92ddbc83f5d
GregsonderGrauwerdene <157585726+GregsonderGrauwerdene@users.noreply.github.com>
2024-01-24 14:51:37 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@cc3e01818eccfdfd8551e3367ddf64d3fbf701a4 🚀

--- assets/klassen.md.FoLMx9O7.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as r,V as t}from"./chunks/framework.mleWmeBo.js";const b=JSON.parse('{"title":"Klassen","description":"","frontmatter":{},"headers":[],"relativePath":"klassen.md","filePath":"klassen.md"}'),o={name:"klassen.md"},s=t("",6),n=[s];function l(i,d,c,h,_,u){return r(),e("div",null,n)}const f=a(o,[["render",l]]);export{b as __pageData,f as default};
