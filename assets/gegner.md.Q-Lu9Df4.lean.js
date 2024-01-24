8b269faad14467d094e72d587c99d78dcdc325c4
GregsonderGrauwerdene <157585726+GregsonderGrauwerdene@users.noreply.github.com>
2024-01-24 20:42:28 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@943133f5e6ef7954a8bc13793724f2bc1bd28a85 🚀

---
+++ assets/gegner.md.Q-Lu9Df4.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as a,V as t}from"./chunks/framework.KHOSnk99.js";const m=JSON.parse('{"title":"Gegner","description":"","frontmatter":{},"headers":[],"relativePath":"gegner.md","filePath":"gegner.md"}'),n={name:"gegner.md"},o=t("",4),s=[o];function i(l,h,_,c,d,g){return a(),r("div",null,s)}const f=e(n,[["render",i]]);export{m as __pageData,f as default};
