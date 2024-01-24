b757b533381ad57b3bbf98b2e883a2a172cb1f18
GregsonderGrauwerdene <157585726+GregsonderGrauwerdene@users.noreply.github.com>
2024-01-24 14:11:07 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@fc0838f1bd037347fc380d11a3ed39dfc3f36728 🚀

---
+++ assets/npc.md.FmaXjCy5.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as d,V as r}from"./chunks/framework.mleWmeBo.js";const _=JSON.parse('{"title":"NPC Liste","description":"","frontmatter":{},"headers":[],"relativePath":"npc.md","filePath":"npc.md"}'),n={name:"npc.md"},l=r("",2),a=[l];function i(c,s,g,o,x,y){return d(),e("div",null,a)}const f=t(n,[["render",i]]);export{_ as __pageData,f as default};
