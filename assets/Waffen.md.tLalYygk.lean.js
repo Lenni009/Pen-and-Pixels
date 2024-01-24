af6b08b4962b9a641d947a03da1608dfe6f11192
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-24 10:05:24 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@47a1a4ea55630b754ececd2f7017e6ebf5e5258d 🚀

--- assets/Waffen.md.tLalYygk.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o as t,V as n}from"./chunks/framework.mleWmeBo.js";const m=JSON.parse('{"title":"Waffen","description":"","frontmatter":{},"headers":[],"relativePath":"Waffen.md","filePath":"Waffen.md"}'),i={name:"Waffen.md"},l=n("",3),f=[l];function o(r,_,s,c,d,p){return t(),a("div",null,f)}const u=e(i,[["render",o]]);export{m as __pageData,u as default};
