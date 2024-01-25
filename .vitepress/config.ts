d39594a1e07fadb2e448cfc2b5d022be13b028b0
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-25 08:47:48 +0100

    moar meta tags

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -14,10 +14,15 @@ export default defineConfig({
     }
   },
   head: [
+    ['meta', { name: 'theme-color', content: '#a8b1ff' }],
+    ['meta', { name: 'msapplication-config', content: '/Studioprojekt2024/icons/browserconfig.xml' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#a8b1ff' }],
+    ['link', { rel: 'mask-icon', href: '/Studioprojekt2024/icons/safari-pinned-tab.svg', color: '#a8b1ff' }],
+    ['link', { rel: 'manifest', href: '/Studioprojekt2024/icons/site.webmanifest' }],
     ['link', { rel: 'icon', sizes: '180x180', href: '/Studioprojekt2024/icons/apple-touch-icon.png' }],
-    ['link', { rel: 'icon', sizes: '32x32', href: '/Studioprojekt2024/icons/favicon-32x32.png' }],
-    ['link', { rel: 'icon', sizes: '16x16', href: '/Studioprojekt2024/icons/favicon-16x16.png' }],
-    ['link', { rel: 'icon', href: '/Studioprojekt2024/icons/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/Studioprojekt2024/icons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/Studioprojekt2024/icons/favicon-16x16.png' }],
+    ['link', { rel: 'shortcut icon', href: '/Studioprojekt2024/icons/favicon.ico' }],
     ['link', { rel: 'stylesheet', href: '/Studioprojekt2024/globalStyle.css' }],
   ],
   themeConfig: {
