f6463037a8ff3478703be0cc414e12e11832f304
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-25 09:59:24 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@46f862a048d83bb89d1b2bb672b7ab0fa715c61f 🚀

---
+++ assets/items.md.tIxraLMM.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,V as n}from"./chunks/framework.j3YL89V9.js";const x=JSON.parse('{"title":"Items","description":"","frontmatter":{},"headers":[],"relativePath":"items.md","filePath":"items.md"}'),s={name:"items.md"},r=n("",2),d=[r];function i(l,c,o,_,m,h){return a(),e("div",null,d)}const p=t(s,[["render",i]]);export{x as __pageData,p as default};
