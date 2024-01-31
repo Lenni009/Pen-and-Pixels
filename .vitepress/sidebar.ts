307bcdf753ed24d3fc878eef38ff6cc2f0769f34
Lenni <guentherhanslphdyt@gmail.com>
2024-01-31 07:10:25 +0000

    rename isPublic to isInternal | closes #4

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -2,11 +2,11 @@ import { CustomSidebarItem } from '../pages/code/types/customSidebar';
 
 // text: der Text, der in der Seitenleiste erscheint
 // link: URL, unter der die Seite erreichbar ist. Muss mit dem Dateinamen der entsprechenden .md Datei übereinstimmen
-// isPublic: wenn "false", wird der Link/Abschnitt nicht während der Aufführungszeit (ab 29.04.2024) angezeigt werden. Kann auf ganzen Abschnitten und auf einzelnen Links verwendet werden
+// isInternal: wenn "true", wird der Link/Abschnitt nicht während der Aufführungszeit (ab 29.04.2024) angezeigt werden. Kann auf ganzen Abschnitten und auf einzelnen Links verwendet werden
 // isPublicEntry: wenn "true", wird dieser Link das Ziel des "Wiki" Links auf der Startseite. Falls es öfter verwendet wird, wird der erste für den Link verwendet
 export const sidebar: CustomSidebarItem[] = [
   {
-    isPublic: false,
+    isInternal: true,
     items: [
       { text: 'Disclaimer', link: '/intro' },
       { text: 'Seite erstellen', link: '/erstellen' },
