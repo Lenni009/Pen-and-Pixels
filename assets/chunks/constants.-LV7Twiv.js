e6c2343d3dcd176ffbe8af83a3806cc9083a9a87
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-01 19:43:27 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@245e930bf52543765ec08a4f57481b95e9b2bc48 🚀

---
+++ assets/chunks/constants.-LV7Twiv.js
@@ -0,0 +1,1 @@
+const s=JSON.parse('{"isShowTime":false}'),i=80,o=20,a=s.isShowTime;export{o as d,a as i,i as m};
--- assets/chunks/constants.YDZXFlAp.js
+++
@@ -1,1 +0,0 @@
-const s=JSON.parse('{"showTime":false}'),o=80,a=20,e=s.showTime;export{a as d,o as m,e as s};
