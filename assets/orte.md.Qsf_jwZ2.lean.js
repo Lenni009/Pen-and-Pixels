a2de2823bb3f3586a799df72640377d509eac62e
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-09 10:58:12 +0000

    Deploying to gh-pages from @ Lenni009/Studioprojekt2024@771912e46ee7abaee1a62b834a256b063763df7a 🚀

---
+++ assets/orte.md.Qsf_jwZ2.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as r,V as n,ap as t,aq as i}from"./chunks/framework.aePlamwM.js";const _=JSON.parse('{"title":"Orte in Gaia","description":"","frontmatter":{},"headers":[],"relativePath":"orte.md","filePath":"orte.md"}'),h={name:"orte.md"},o=n("",16),l=[o];function d(s,c,u,b,m,f){return r(),e("div",null,l)}const q=a(h,[["render",d]]);export{_ as __pageData,q as default};
