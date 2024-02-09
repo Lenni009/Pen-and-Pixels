8a3068c4cf391a963745633aa48effbb8f5b0753
Lenni <guentherhanslphdyt@gmail.com>
2024-02-09 09:54:15 +0100

    Update barde.ts

--- pages/characterSheetData/barde.ts
+++ pages/characterSheetData/barde.ts
@@ -89,10 +89,7 @@ const characterData: CharacterSheet = reactive({
   statur: 'Elbenhaft',
   beruf: 'Barde',
   gesinnung: 'Chaotic Neutral',
-  anmerkungen: `
-  Schizophren
-  
-  Zauberspruch: Der braune Ton`,
+  anmerkungen: 'Schizophren / Zauberspruch: Der braune Ton',
   handeln,
   wissen,
   soziales,
