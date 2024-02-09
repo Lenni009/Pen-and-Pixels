4c51a4b775538d77b0403e357c332c328b2513ba
Lenni <guentherhanslphdyt@gmail.com>
2024-02-09 10:39:40 +0100

    use template

--- pages/characterSheetData/barde.ts
+++ pages/characterSheetData/barde.ts
@@ -89,7 +89,8 @@ const characterData: CharacterSheet = reactive({
   statur: 'Elbenhaft',
   beruf: 'Barde',
   gesinnung: 'Chaotic Neutral',
-  anmerkungen: 'Schizophren\nZauberspruch: Der braune Ton',
+  anmerkungen: `Schizophren
+  Zauberspruch: Der braune Ton`,
   handeln,
   wissen,
   soziales,
