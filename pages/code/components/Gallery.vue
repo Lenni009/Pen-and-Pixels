fef2b708acd7bedc1b662da6af5d262ac03a38ad
Lenni009 <guentherhanslphdyt@gmail.com>
2024-05-01 23:05:01 +0200

    add jump buttons

--- pages/code/components/Gallery.vue
+++ pages/code/components/Gallery.vue
@@ -10,7 +10,21 @@ const selected = ref(0);
 
 <template>
   <div class="gallery-container">
-    <div>
+    <div class="main-image-container">
+      <button
+        v-if="selected"
+        class="jump-btn back"
+        @click="selected--"
+      >
+        <
+      </button>
+      <button
+        v-if="selected !== items.length - 1"
+        class="jump-btn next"
+        @click="selected++"
+      >
+        >
+      </button>
       <img
         :src="items[selected]"
         alt="Behind the Scenes"
@@ -20,11 +34,13 @@ const selected = ref(0);
     <div class="gallery">
       <div
         v-for="(image, index) in items"
+        :class="{ selected: selected === index }"
         class="img"
       >
         <img
           :src="image"
           alt="Behind the Scenes"
+          loading="lazy"
           @click="selected = index"
         />
       </div>
@@ -38,18 +54,49 @@ const selected = ref(0);
   flex-direction: column;
   gap: 0.25rem;
 
-  .main-img {
-    width: auto;
-    max-height: calc(100svh - 200px);
+  .main-image-container {
+    align-content: center;
+    position: relative;
+
+    .jump-btn {
+      position: absolute;
+      bottom: 50%;
+      aspect-ratio: 1;
+      font-size: large;
+      left: 1rem;
+      border-radius: 50%;
+      background-color: #52525294;
+      color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+
+      &.next {
+        right: 1rem;
+        left: unset;
+      }
+    }
+
+    .main-img {
+      width: auto;
+      max-height: calc(100svh - 200px);
+    }
   }
 
   .gallery {
     display: flex;
-    gap: 0.25rem;
+    gap: 0.15rem;
     overflow-x: scroll;
+    scroll-behavior: smooth;
 
     .img {
       cursor: pointer;
+      border: 1px solid transparent;
+
+      &.selected {
+        border-color: var(--vp-c-text-1);
+      }
 
       img {
         max-height: 100px;
